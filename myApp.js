ac5a73be747101cff511d7b69899c3a5d399aece
Avi Mukesh <avimukesh10@gmail.com>
2021-01-03 16:52:23 +0000

    Changed spelling of favourite

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopolog
 const personSchema = new mongoose.Schema({
   name: {type: String, required: true},
   age: Number,
-  favouriteFoods: [String]
+  favoriteFoods: [String]
 })
 
 const Person = mongoose.model('Person', personSchema);
