b9e6375e3c1aa0b41a504959701d0fa73c491d92
Avi Mukesh <avimukesh10@gmail.com>
2021-01-03 16:49:59 +0000

    Fixed typo in Person model

--- myApp.js
+++ myApp.js
@@ -9,12 +9,14 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopolog
 const personSchema = new mongoose.Schema({
   name: {type: String, required: true},
   age: Number,
-  favouriteFood: [String]
+  favouriteFoods: [String]
 })
 
 const Person = mongoose.model('Person', personSchema);
 
+
 const createAndSavePerson = (done) => {
+
   done(null /*, data*/);
 };
 
