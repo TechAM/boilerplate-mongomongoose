9c005172df86bb0db001c556557b73610ee6fa60
Avi Mukesh <avimukesh10@gmail.com>
2021-01-04 18:41:14 +0000

    commented out require('dotenv')

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-var mongoose = require('mongoose')
+// require('dotenv').config();
 
+var mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:false})
 
 const personSchema = new mongoose.Schema({
